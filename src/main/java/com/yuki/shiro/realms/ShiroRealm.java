1f4a9440494a04113a8ae91e71acf53a83cb3ff3
liao1565553 <949395746@qq.com>
2017-03-31 23:52:01 +0800

    md5加密还不够，md5盐值加密

--- src/main/java/com/yuki/shiro/realms/ShiroRealm.java
+++ src/main/java/com/yuki/shiro/realms/ShiroRealm.java
@@ -11,6 +11,7 @@ import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.crypto.hash.SimpleHash;
 import org.apache.shiro.crypto.hash.SimpleHashRequest;
 import org.apache.shiro.realm.AuthenticatingRealm;
+import org.apache.shiro.util.ByteSource;
 
 import com.yuki.account.dto.User;
 import com.yuki.account.service.UserService;
@@ -18,33 +19,34 @@ import com.yuki.account.service.UserService;
 public class ShiroRealm extends AuthenticatingRealm {
 	@Resource
 	UserService userService;
+
 	@Override
-	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) 
-			throws AuthenticationException {
-		System.out.println("doGetAuthenticationInfo:"+token);
-		
-		//1.把AuthenticationToken转化为UsernamePasswordToken
-		UsernamePasswordToken upToken=(UsernamePasswordToken)token;
-		
-		//2.从UsernamePasswordToken获取username
-		String username=upToken.getUsername();
-		
-		//3.调用数据库的方法，从数据库中查询username,对应的信息
-		User user=userService.getUserByUsername(username);
-		
-		//4.如果用户不存在，则可以抛出UnknownAccountException
-		if (user==null) {
+	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+		System.out.println("doGetAuthenticationInfo:" + token);
+
+		// 1.把AuthenticationToken转化为UsernamePasswordToken
+		UsernamePasswordToken upToken = (UsernamePasswordToken) token;
+
+		// 2.从UsernamePasswordToken获取username
+		String username = upToken.getUsername();
+
+		// 3.调用数据库的方法，从数据库中查询username,对应的信息
+		User user = userService.getUserByUsername(username);
+
+		// 4.如果用户不存在，则可以抛出UnknownAccountException
+		if (user == null) {
 			throw new UnknownAccountException("用户不存在!");
 		}
-		
-		//5.根据用户信息的情况决定是否抛出 用户被锁定异常
-		
-		//6.根据用户信息 构建
-		Object principal=user;//认证信息，可以是username，也可以是数据表中对应的实体类对象如user
-		Object credentials=user.getPassword();//数据库中的密码
-		String realmName=getName();//当前realm的name
-		
-		SimpleAuthenticationInfo info=new SimpleAuthenticationInfo(principal, credentials, realmName);
+
+		// 5.根据用户信息的情况决定是否抛出 用户被锁定异常
+
+		// 6.根据用户信息 构建
+		Object principal = user;// 认证信息，可以是username，也可以是数据表中对应的实体类对象如user
+		Object credentials = user.getPassword();// 数据库中的密码
+		String realmName = getName();// 当前realm的name
+		ByteSource credentialsSalt =ByteSource.Util.bytes(username);//用用户的username作为盐值进行盐值加密
+
+		SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(principal, credentials, credentialsSalt, realmName);
 		return info;
 	}
 }
--- src/test/java/com/yuki/test/MD5Test.java
+++ src/test/java/com/yuki/test/MD5Test.java
@@ -1,10 +1,12 @@
 package com.yuki.test;
 
 import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.util.ByteSource;
 
 public class MD5Test {
 	public static void main(String[] args) {
-		Object result= new  SimpleHash("MD5", "21232f297a57a5a743894a0e4a801fc3", null, 3);
+		ByteSource credentialsSalt =ByteSource.Util.bytes("admin");
+		Object result= new  SimpleHash("MD5", "21232f297a57a5a743894a0e4a801fc3",credentialsSalt, 3);
 		System.out.println(result);
 	}
 }
